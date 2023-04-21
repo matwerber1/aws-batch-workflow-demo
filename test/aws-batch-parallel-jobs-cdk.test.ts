30ac80d11a46df1dfe78761d75ddb80fde4e3cc5
Mat Werber <matwerber@gmail.com>
2023-04-20 18:49:42 -0700

    Initial commit

---
+++ test/aws-batch-parallel-jobs-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsBatchParallelJobsCdk from '../lib/aws-batch-parallel-jobs-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-batch-parallel-jobs-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsBatchParallelJobsCdk.AwsBatchParallelJobsCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
